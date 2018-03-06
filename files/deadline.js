b01930541c5fa6ae8afa44b602b85a9a652c63b5
Isengar <ricci.gr@gmail.com>
2018-03-06 16:40:53 -0300

    files with json request examples

---
+++ files/deadline.js
@@ -0,0 +1,5 @@
+{
+	"nCdServico" : "40010",
+	"sCepOrigem": "03980150",
+	"sCepDestino" : "08062540"
+}
---
+++ files/payment.js
@@ -0,0 +1,15 @@
+{
+	"payment": {
+		"method":"card",
+		"value": 100,
+		"currency":  "USD",
+		"description": "testing card"
+	},
+	"card": {
+	  "number":1234123412341234,
+	  "scheme":"visa",
+	  "expiration_year":2016,
+	  "expiration_month":12,
+	  "cvv":123
+	}
+}
